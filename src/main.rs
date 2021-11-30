5ba0b1c70b1805e0cda7f0b418b9e6849f6c07a1
Fabian Krautgasser <fkrautgasser@posteo.org>
2021-11-30 14:24:38 +0100

    MNT: rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::process::Command;
-use serde::{Serialize};
+use serde::Serialize;
 use serde_json::Result;
+use std::process::Command;
 
 fn serialize_cmd_output(infos: &[Cmd]) -> Result<()> {
     println!("{}", serde_json::to_string(infos)?);
@@ -16,33 +16,30 @@ struct Cmd {
 }
 
 fn main() {
-
     let cmds = vec![
-        "python -V", 
+        "python -V",
         "python2 -V",
         "conda -V",
-        "docker version --format 'client: {{ .Client.Version }}, api: {{ .Client.APIVersion }}'", 
-        
+        "docker version --format 'client: {{ .Client.Version }}, api: {{ .Client.APIVersion }}'",
     ];
-    
+
     let mut infos: Vec<Cmd> = Vec::new();
 
     for cmd in cmds {
         let output = Command::new("sh")
-                             .arg("-c")
-                             .arg(cmd)
-                             .output()
-                             .expect("Could not executer process");  // this one only panics if `sh` does not spawn
+            .arg("-c")
+            .arg(cmd)
+            .output()
+            .expect("Could not executer process"); // this one only panics if `sh` does not spawn
         let cmd_parts: Vec<&str> = cmd.split(' ').collect();
-        let info = Cmd { 
+        let info = Cmd {
             cmd: String::from(cmd_parts[0]),
             stdout: String::from(String::from_utf8(output.stdout).unwrap().trim()),
             stderr: String::from(String::from_utf8(output.stderr).unwrap().trim()),
-            status: output.status.code().unwrap()
+            status: output.status.code().unwrap(),
         };
         infos.push(info);
     }
-    
+
     let _result = serialize_cmd_output(&infos);
 }
-
